3ffac47c14297703f606cb71ec0b34bc1695278a
twdeveloper <twdeveloper@outlook.com>
2019-07-28 17:29:24 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# LineBot-GetQuota
+# LineBot Example - GetQuota
 Get LINE Bot message quota,  delivery status, and Insight
+
+This example shows how to use the LineBotSDK to get the message delivery status.
+
+including of the following APIs:
+1. Get the target limit for additional messages
+2. Get number of messages sent this month
+3. Get number of sent reply messages
+4. Get number of sent push messages
+5. Get number of sent multicast messages
+6. Get number of sent broadcast messages
+7. Get number of message deliveries
+8. Get number of followers
+9. Get friend demographics
+
+# resource:
+LineBotSDK：https://www.nuget.org/packages/LineBotSDK  
+相關課程：http://www.studyhost.tw/NewCourses/LineBot   
+線上課程：https://www.udemy.com/line-bot/   
+更多內容，請參考電子書：https://www.pubu.com.tw/ebook/103305    
+LINE Bot 實體書籍：https://www.tenlong.com.tw/products/9789865020354
---
+++ main/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using isRock.LineBot.Extensions;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Channel Access Token : ");
+            var CAT = Console.ReadLine();
+            isRock.LineBot.Bot b = new isRock.LineBot.Bot(CAT);
+
+            var msg = "";
+
+            msg += $"\n\n Get the target limit for additional messages : \n{b.GetTheTargetLimitForAdditionalMessages().value}";
+            msg += $"\n\n Get number of messages sent this month : \n{b.GetNumberOfMessagesSentThisMonth().totalUsage}";
+
+            msg += $"\n\n Get number of sent reply messages : \n{b.GetNumberOfSentReplyMessages(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+            msg += $"\n\n Get number of sent push messages : \n{b.GetNumberOfSentPushMessages(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+            msg += $"\n\n Get number of sent multicast  messages : \n{b.GetNumberOfSentMulticastMessages(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+            msg += $"\n\n Get number of sent broadcast  messages : \n{b.GetNumberOfSentBroadcastMessages(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+
+            msg += $"\n\n Get number of message deliveries : \n{b.GetNumberOfMessageDeliveries(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+            msg += $"\n\n Get number of followers : \n{b.GetNumberOfFollowers(DateTime.UtcNow.AddDays(-1)).ToJson()}";
+            msg += $"\n\n Get friend demographics : \n{b.GetFriendDemographics().ToJson()}";
+
+            Console.WriteLine(msg);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
